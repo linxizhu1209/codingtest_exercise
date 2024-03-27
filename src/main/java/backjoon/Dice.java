662a97c6de5754faac2332cae409756b88a3b0b3
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-27 16:21:18 +0900

    <백준 - 주사위 세개> - 주사위 3개를 던져서 어떤 눈이 나오냐에 따라 받을 수 있는 상금을 리턴하는 문제 - 모두 같은 눈이 나오면 10000+(같은 눈)*1000 - 모두 다른 눈이 나오면 가장큰눈 * 100 - 두개씩만 같은 눈이 나오면 1000+(같은눈*10) - if문으로 여러가지 조건을 설정하고 출력한다.

---
+++ src/main/java/backjoon/Dice.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Dice {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int l = in.nextInt();
+        if (n != m && n != l && m != l) {
+            int max = Math.max(n, m);
+            System.out.println(100 * Math.max(max, l));
+        } else if (n == m && n != l) System.out.println(1000 + (n * 100));
+        else if (n == l && n != m) System.out.println(1000 + (n * 100));
+        else if (m == l && m != n) System.out.println(1000 + (m * 100));
+        else System.out.println(10000 + (n * 1000));
+    }
+}
