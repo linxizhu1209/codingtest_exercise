d67385d01e5c87aac8a6fba3bd8ceea205aaa4c9
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-27 14:30:47 +0900

    <백준 - 2007년> - 2007년의 1월1일은 월요일이고, 만약 임의의 날짜가 주어질때 해당 날짜는 무슨요일인가 도출하는 문제 - Map 두개를 형성하고, 하나는 날짜를 7로나누었을때의 나머지와 요일 저장, 하나는 월과 해당 월의 마지막날짜를 저장 - 구해야하는 날짜가 주어지면, 월은 M, 일은 D에 할당한 후 for문으로 해당 월의 전월까지의 일수를 합한다. for문이 종료되면 합한 총일수 + 주어진 D를 더하면 해당날짜가 365일중 몇일째인지 값이나온다. - 해당 값을 7로 나눈 나머지를 요일이 저장되어있는 date Map에서 찾아와서 리턴

--- src/main/java/backjoon/whatDay.java
+++ src/main/java/backjoon/whatDay.java
@@ -1,8 +1,31 @@
 package backjoon;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class whatDay {
     public static void main(String[] args) {
+        Map<Integer,String> date = new HashMap<>();
+        date.put(1,"MON");date.put(2,"TUE");
+        date.put(3,"WED");date.put(4,"THU");
+        date.put(5,"FRI");date.put(6,"SAT");
+        date.put(0,"SUN");
 
-
+        Map<Integer,Integer> totalDay = new HashMap<>();
+        totalDay.put(1,31); totalDay.put(2,28); totalDay.put(3,31);
+        totalDay.put(4,30); totalDay.put(5,31); totalDay.put(6,30);
+        totalDay.put(7,31); totalDay.put(8,31); totalDay.put(9,30);
+        totalDay.put(10,31); totalDay.put(11,30); totalDay.put(12,31);
+        Scanner in = new Scanner(System.in);
+        int M = in.nextInt();
+        int D = in.nextInt();
+        int sum = 0;
+        for(int i=1;i<M;i++){
+            sum+= totalDay.get(i);
+        }
+        int today = sum+D;
+        String todayDate = date.get(today%7);
+        System.out.println(todayDate);
     }
 }
