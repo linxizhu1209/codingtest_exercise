9757f33fbe24763dc7ab73b9d6f675eb701ed1b3
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-27 11:36:02 +0900

    <백준 - 별찍기8> - 숫자 n이 주어지면 첫째줄부터 2*n-1번째 줄까지 차례대로 별을 출력하는 것. 다만, 별은 대칭을 이뤄야한다 - 별과 공백을 순서만 바꿔서 두번 출력해주면 된다. 또한, 뒤집기 위해 for문을 한 번 더 돌리되 별을 다 채운 경우. 즉 n번째줄은 한번만 반복하도록 한다

---
+++ src/main/java/backjoon/star8.java
@@ -0,0 +1,24 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class star8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=1;i<=n;i++){
+             System.out.print("*".repeat(i));
+             System.out.print(" ".repeat(n-i));
+             System.out.print(" ".repeat(n-i));
+             System.out.print("*".repeat(i));
+             System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            System.out.print("*".repeat(i));
+            System.out.print(" ".repeat(n-i));
+            System.out.print(" ".repeat(n-i));
+            System.out.print("*".repeat(i));
+            System.out.println();
+        }
+    }
+}
