e290b12c29ecc064bcfbab71890b4d8132398544
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-27 10:43:04 +0900

    <백준 - 별찍기 2> - 숫자 n이 주어졌을때 첫째줄에는 별 1개, 둘째줄에는 별2개~ n번째줄에는 별n개를 찍는 문제. 다만, 오른쪽을 기준으로 정렬 - 오른쪽을 기준으로 정렬해야하므로 이중for문을 돌며, 하나는 공백 찍는 용도, 하나는 별찍는 용도로 사용한다. 이때 공백은 n-i개를 찍어야한다.

---
+++ src/main/java/backjoon/star2.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class star2 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=n-i-1;j>=0;j--){
+                System.out.print(" ");
+            }
+            System.out.print("*".repeat(i));
+            System.out.println();
+        }
+    }
+}
