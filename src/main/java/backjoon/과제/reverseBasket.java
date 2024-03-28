ff24e04c93073b200621b181fec49800d07cab55
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-28 15:20:16 +0900

    <백준 - 바구니 뒤집기> - 바구니의 개수 n, 뒤집는 횟수 m이 주어지고 m번에 걸쳐 뒤집는 경우가 입력될때 뒤집어진 결과물을 도출하는 문제 - StringBuilder의 reverse로 하려고 했으나, 바구니의 숫자가 10이상일때 숫자를 문자열에 넣으면 10으로 인식되는게 아니라 1과0으로 인식되기에 불가능

---
+++ src/main/java/backjoon/과제/reverseBasket.java
@@ -0,0 +1,55 @@
+package backjoon.과제;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class reverseBasket {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer token;
+        token = new StringTokenizer(bf.readLine());
+        int basket = Integer.parseInt(token.nextToken());
+        int count = Integer.parseInt(token.nextToken());
+        int[] arr = new int[basket + 1];
+        for (int i = 1; i <= basket; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < count; i++) {
+            token = new StringTokenizer(bf.readLine());
+            int n = Integer.parseInt(token.nextToken());
+            int m = Integer.parseInt(token.nextToken());
+            while (n <= m) {
+                int tmp = arr[n];
+                arr[n] = arr[m];
+                arr[m] = tmp;
+                n++;
+                m--;
+            }
+        }
+            for (int x = 1; x <= basket; x++) {
+                bw.write(arr[x] + " ");
+            }
+            bw.close();
+    }
+}
+            // 반례 : 10 1 과 같이 입력했을때, answer 에 10이 1과 0으로 인식되기에 안됨
+
+            //        String answer = "";
+//        for(int i=1;i<=basket;i++) answer+=i;
+//        StringBuilder sb;
+//        for(int i=1;i<=count;i++){
+//            token = new StringTokenizer(bf.readLine());
+//            int n = Integer.parseInt(token.nextToken());
+//            int m = Integer.parseInt(token.nextToken());
+//            String sub= answer.substring(n-1,m);
+//            String reversesub= new StringBuilder(sub).reverse().toString();
+//            answer = answer.replace(sub,reversesub);
+//        }
+//        for(char x:answer.toCharArray()){
+//            bw.write(x+" ");
+//        }
+//        bw.close();
+//    }
+//}
