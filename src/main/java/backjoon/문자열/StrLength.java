e7b16c7e6387dbb7ba5095422f59adfa8bdfc1d3
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-28 16:23:42 +0900

    <백준 - 문자열> - n개의 문자열이 주어지면 해당 문자열의 앞과 끝 문자를 출력하는 문제 - 문자열의 맨앞 문자는 0번째인덱스, 마지막 문자는 길이-1 인덱스이므로 charAt을 사용하여 출력

---
+++ src/main/java/backjoon/문자열/StrLength.java
@@ -0,0 +1,13 @@
+package backjoon.문자열;
+
+import java.io.*;
+
+public class StrLength {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+        bw.write(s.length()+"\n");
+        bw.close();
+    }
+}
