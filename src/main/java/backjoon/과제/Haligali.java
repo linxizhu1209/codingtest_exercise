10139bfcb972cb664f714b8ed217bb8591068491
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-28 14:11:09 +0900

    <백준 - 할리갈리> - 같은 종류의 과일이 5개가 있는 경우 YES출력, 아니면 NO출력 - MAP을 만들어 과일이름과 개수를 저장하고, Values를 for문으로 돌며 5개가 있는지 확인한다.

---
+++ src/main/java/backjoon/과제/Haligali.java
@@ -0,0 +1,31 @@
+package backjoon.과제;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Haligali {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n =Integer.parseInt(bf.readLine());
+        Map<String,Integer> fruitMap = new HashMap<>();
+        StringTokenizer token;
+        String answer = "NO";
+        for(int i=0;i<n;i++) {
+             token = new StringTokenizer(bf.readLine());
+             String fruit = token.nextToken();
+             int num = Integer.parseInt(token.nextToken());
+            fruitMap.put(fruit, fruitMap.getOrDefault(fruit,0)+num);
+        }
+        for(int x:fruitMap.values()){
+            if(x==5) {
+                answer="YES";
+                break;
+            }
+        }
+        bw.write(answer);
+        bw.close();
+    }
+    }
