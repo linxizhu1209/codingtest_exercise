5a3a20c95d0e8956ed41bf5e52b52a4a75365091
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-28 09:09:31 +0900

    <백준 - 상수> - 숫자 두개가 주어지면 거꾸로 뒤집었을때 더 큰 숫자를 출력하는 문제 - StringBuilder의 reverse메서드를 사용해 받아온 문자열을 뒤집고, 비교

---
+++ src/main/java/backjoon/과제/ReverseNum.java
@@ -0,0 +1,18 @@
+package backjoon.과제;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class ReverseNum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String sb = new StringBuilder(bf.readLine()).reverse().toString();
+        StringTokenizer token = new StringTokenizer(sb);
+        int n = Integer.parseInt(token.nextToken());
+        int m = Integer.parseInt(token.nextToken());
+        if(n>m) bw.write(n+"\n");
+        else bw.write(m+"\n");
+        bw.close();
+    }
+}
