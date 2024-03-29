bb50eec46fce9efc9e4cb3481187206f590b27d9
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-29 14:08:00 +0900

    <백준 - 알고리즘 수업:알고리즘의 수행시간 2> - 코드에 주석처리한 것과 같이 코드가 짜여져있을때, 이 알고리즘의 수행횟수는 n번이다. O(n)으로 표현할 수 있다.

---
+++ src/main/java/backjoon/시간복잡도/Algorithm2.java
@@ -0,0 +1,21 @@
+package backjoon.시간복잡도;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Algorithm2 {
+    /**
+     *MenOfPassion(A[], n) {
+     *     sum <- 0;
+     *     for i <- 1 to n
+     *         sum <- sum + A[i]; # 코드1
+     *     return sum;
+     * }
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        System.out.println(n);
+        System.out.println(1);
+    } }
