2fda287172a8c38f03630f33b64109899b767873
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-29 14:21:24 +0900

    <백준 - 알고리즘 수업:알고리즘의 수행시간 3> - 코드에 주석처리한 것과 같은 알고리즘을 푸는 경우, O(N^2)을 수행한다. 즉 수행 횟수는 N제곱이고 이에 차수는 2이다. 다만, N을 int형이아닌 Long형으로 받아야지 오버플로우 발생이 일어나지 않는 다는 걸 명심하자.

---
+++ src/main/java/backjoon/시간복잡도/Algorithm3.java
@@ -0,0 +1,23 @@
+package backjoon.시간복잡도;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * MenOfPassion(A[], n) {
+ *     sum <- 0;
+ *     for i <- 1 to n
+ *         for j <- 1 to n
+ *             sum <- sum + A[i] × A[j]; # 코드1
+ *     return sum;
+ * }
+ */
+public class Algorithm3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        Long n = Long.parseLong(bf.readLine());
+        System.out.println(n*n);
+        System.out.println(2);
+    }
+}
