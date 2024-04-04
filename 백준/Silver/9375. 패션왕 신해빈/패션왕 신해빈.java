422210d3e5bb9ef6ff4d1d6558944cddbb01f431
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-04-04 18:24:08 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 148 ms, Memory: 14612 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,22 +1,18 @@
 import java.io.*;
 import java.util.HashMap;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
+
         for(int i=0;i<n;i++){
             HashMap<String,Integer> map = new HashMap<>();
             int x = Integer.parseInt(br.readLine());
-            StringTokenizer token;
-            while(x!=0){
-                token = new StringTokenizer(br.readLine());
-                token.nextToken();
-                String type = token.nextToken();
-                map.put(type,map.getOrDefault(type,0)+1);
-                x--;
+            for(int j=0;j<x;j++){
+                String[] str = br.readLine().split(" ");
+                map.put(str[1],map.getOrDefault(str[1],0)+1);
             }
             int sum = 1;
             for (int o : map.values()) {
