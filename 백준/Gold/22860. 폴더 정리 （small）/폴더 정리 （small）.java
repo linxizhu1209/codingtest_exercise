6b5ae791f30b493fc84793e68c7dc7b6d9482a7e
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2025-03-26 16:39:32 +0900

    [Gold III] Title: 폴더 정리 (small), Time: 1368 ms, Memory: 167484 KB -BaekjoonHub

--- 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.java
+++ 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -23,16 +24,9 @@ public class Main {
         int N = Integer.parseInt(token.nextToken());
         int M = Integer.parseInt(token.nextToken());
         int s = N+M;
-        List<String> list = new ArrayList<>();
         Map<String,List<isFile>> map = new HashMap<>();
         for(int i=0;i<s;i++){
             token = new StringTokenizer(in.nextLine());
-            /**
-             * 첫번째 토큰, 상위 폴더의 이름 P
-             * 두번째 토큰, 폴더 또는 파일 이름 F
-             * 세번째 토큰, 폴더 여부 C (폴더면 1, 파일이면 0)
-             * 문제는 쿼리 경로가 주어지면, 그 경로 아래에 있는 파일 종류의 개수와 파일의 종류와 파일 총 개수 (파일의 종류같은 경우에는 File1이 A폴더에도 있고 B폴더에도 있을 수 있음)
-             */
             String p = token.nextToken();
             String f = token.nextToken();
             int c = Integer.parseInt(token.nextToken());
@@ -40,11 +34,6 @@ public class Main {
             List<isFile> fileList = map.getOrDefault(p,new ArrayList<>());
             fileList.add(new isFile(f,c));
             map.put(p,fileList);
-            // 맵 키값을 상위 폴더명, 맵 value를 리스트로 하고, list.add("FolderA","FolderB", "File1") 이런식으로 하고
-            // 쿼리를 가져올때에는 "/"를 기준으로 마지막 단어만 가져오고 그 단어와 일치하는 key를 map에서 찾고, 걔 안에 있는 list 하나씩 돌면서 만약 file이면 count(총개수)++
-            // 중복불가 set도 하나 만들어서, 파일 set에 하나씩 넣고, 마지막에 그 개수 (종류 개수) 출력
-            // 만약 폴더라면 그 폴더도 똑같이 돌아야함.
-            // 근데 파일이름과 폴더이름이 file~ folder~ 꼭 이런건 아니니까 구분할 수있는 무언가 있어야할듯
         }
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -85,9 +74,6 @@ public class Main {
             kindCount = fileName.size();
             bw.write(kindCount+" "+count+"\n");
         }
-        /**
-         * 1. 첫번째줄 폴더 개수 N, 파일 개수 M
-         */
         bw.close();
     }
 }
