e7dd63600e47b4d600214160391b25c5b61ab852
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2025-07-17 15:41:40 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 192 ms, Memory: 18024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,12 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner in  = new Scanner(System.in);
+        while (in.hasNextLine()){
+            System.out.println(in.nextLine());
+        }
+    }
+}
