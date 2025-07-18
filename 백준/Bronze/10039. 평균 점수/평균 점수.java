67f2a40ac3d445959d26c5f1ebda7e02fa84a6c0
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2025-07-18 09:43:26 +0900

    [Bronze IV] Title: 평균 점수, Time: 96 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for(int i=0;i<5;i++){
+            int x = Integer.parseInt(br.readLine());
+            if(x<40) x=40;
+            sum+=x;
+        }
+        System.out.println(sum/5);
+
+
+    }
+}
